7e17c391736f89f31045086677a139b95734d229
Haziq Naeem <haziqnaeem36@gmail.com>
2022-03-08 13:03:30 -0500

    heading style

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -63,42 +63,6 @@ function createHouse(req, res) {
 
 
 
-
-    // function edit(req, res) {
-    //   House.findById(req.params.id)
-    //   .then(house => {
-    //     res.render("profiles/edit", {
-    //       house,
-    //       title: "edit house"
-    //     })
-    //   })
-    //   .catch(err => {
-    //     console.log(err)
-    //     res.redirect("/profiles")
-    //   })
-    // }
-
-    // function update(req, res) {
-    //   req.body.owner = req.user.profile._id
-    //   Profile.findById(req.params.id)
-    //   .then(house => {
-    //     if (house.owner.equals(req.user.profile._id)) {
-    //       house.updateOne(req.body, {new: true})
-    //       .then(() => {
-    //         res.redirect(`/profiles/${req.user.profile._id}`)
-    //       })
-    //     } else {
-    //       throw new Error("NOT AUTHORIZED")
-    //     }
-    //   })
-    //   .catch(err => {
-    //     console.log("the error:", err)
-    //     res.redirect("/profiles")
-    //   })
-    // }
-
-
-
 export {
   show,
   createHouse,
